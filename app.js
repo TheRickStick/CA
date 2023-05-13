97f21f6cedfe37bb03b5a5072165d666b3df282b
TheRickStick <114582372+TheRickStick@users.noreply.github.com>
2023-05-13 20:05:54 +0200

    24

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var participantsRouter = require('./routes/participants');
-var participantDetails = require('./routes/participantDetails');
+
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/participants', participantsRouter);
-app.use('/participants', participantDetails);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
